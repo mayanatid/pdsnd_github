54152392cfa8ab1f880dab21640d63080dde5093
Mayan Atid <mayanatid@gmail.com>
2019-07-07 18:49:29 -0700

    Added title comment in bikeshare.py file

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+# This is the code for the bikeshare project
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
